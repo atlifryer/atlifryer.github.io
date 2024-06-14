2a26e88444aaa18926224d66371f18327d7aa220
atlifryer <atli1902@gmail.com>
2024-06-14 02:44:24 -0400

    fix postgresql pw

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import SignUpView
+from .views import SignUpView, send_test_email
 
 urlpatterns = [
+    path('send-test-email/', send_test_email, name='send_test_email'),
     path("signup/", SignUpView.as_view(), name="signup"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,7 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
+from django.core.mail import send_mail
+from django.http import HttpResponse
 
 from .forms import CustomUserCreationForm
 
@@ -8,3 +10,13 @@ class SignUpView(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy("login")
     template_name = "registration/signup.html"
+
+def send_test_email(request):
+    send_mail(
+        'Test Email',
+        'This is a test email sent from Django.',
+        'atlifreyr666@gmail.com',
+        ['recipient@example.com'],
+        fail_silently=False,
+    )
+    return HttpResponse('Email sent!')
